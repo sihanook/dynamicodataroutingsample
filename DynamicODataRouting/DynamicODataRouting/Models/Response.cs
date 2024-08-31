901bfda9547d821419df56f7e7136f1daadfdc7e
Sihanook Fernando (from Dev Box) <sifernan@microsoft.com>
2024-08-31 11:34:11 -0700

    One approach to solve it, but this has problems.

---
+++ DynamicODataRouting/DynamicODataRouting/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace DynamicODataRouting.Models
+{
+    public class Response
+    {
+        public IQueryable Content { get; set; }
+
+        public ICollection<string> Metadata { get; set; }
+    }
+}
